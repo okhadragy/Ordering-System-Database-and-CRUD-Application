454e8fb95c8c005cf9f4c80a1bd3ff0461f93ef9
okhadragy <okhadragy@gmail.com>
2025-04-26 02:01:42 +0300

    Added delete and make the prk column unchangeable

--- Project_DB/Form1.cs
+++ Project_DB/Form1.cs
@@ -25,7 +25,8 @@ namespace Project_DB
         public Form1()
         {
             InitializeComponent();
-            con = new SqlConnection("Data Source=LAPTOP-VOK6VQOD;Initial Catalog=\"Ordering System\";Integrated Security=True");
+            con = new SqlConnection("Data Source=LAPTOP-68C2HGIQ;Initial Catalog=\"Ordering System\";Integrated Security=True;");
+            dataGridView.SelectionChanged += dataGridView_SelectionChanged;
 
             comboBox.Text = "Select Table";
             comboBox.Items.Add("Cart");
@@ -39,6 +40,8 @@ namespace Project_DB
             comboBox.Items.Add("Restaurant_Address");
             comboBox.Items.Add("UserTable");
         }
+
+        private List<string> primaryKeys = new List<string>();
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -91,6 +94,18 @@ namespace Project_DB
 
             con.Open();
 
+            // Get primary key columns for the selected table
+            SqlCommand pkCmd = new SqlCommand("EXEC sp_pkeys '" + comboBox.SelectedItem.ToString() + "'", con);
+            SqlDataReader pkReader = pkCmd.ExecuteReader();
+
+            primaryKeys = new List<string>();
+
+            while (pkReader.Read())
+            {
+                primaryKeys.Add(pkReader["COLUMN_NAME"].ToString());
+            }
+            pkReader.Close();
+
             cmd = new SqlCommand("SELECT * FROM " + comboBox.SelectedItem.ToString(), con);
             rdr = cmd.ExecuteReader();
 
@@ -103,8 +118,17 @@ namespace Project_DB
             {
                 checkedListBox.Items.Add(rdr.GetName(i));
                 checkedListBox.SetItemChecked(i, true);
-
                 orderBox.Items.Add(rdr.GetName(i));
+                if (primaryKeys.Contains(rdr.GetName(i)))
+                {
+                    checkedListBox.ItemCheck += (sender2, ev) =>
+                    {
+                        if (primaryKeys.Contains(checkedListBox.Items[ev.Index].ToString()) && ev.NewValue == CheckState.Unchecked)
+                        {
+                            ev.NewValue = CheckState.Checked;
+                        }
+                    };
+                }
             }
 
             con.Close();
@@ -114,15 +138,6 @@ namespace Project_DB
         // prevent read button when no columns are checked
         private void checkedListBox_ItemCheck(object sender, ItemCheckEventArgs e)
         {
-            int newCheckedCount = checkedListBox.CheckedItems.Count;
-
-            // if checking a new item
-            if (e.NewValue == CheckState.Checked)
-                newCheckedCount++;
-            else if (e.NewValue == CheckState.Unchecked)
-                newCheckedCount--;
-
-            readButton.Enabled = newCheckedCount > 0;
         }
 
         private void orderBox_SelectedIndexChanged(object sender, EventArgs e)
@@ -141,6 +156,67 @@ namespace Project_DB
 
                 ascRadio.Enabled = false;
                 descRadio.Enabled = false;
+            }
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count > 0 && !dataGridView.SelectedRows[0].IsNewRow)
+            {
+                deleteButton.Enabled = true;
+            }
+            else
+            {
+                deleteButton.Enabled = false;
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+
+
+                foreach (DataGridViewRow row in dataGridView.SelectedRows)
+                {
+                    if (row.IsNewRow) continue;
+
+                    string deleteQuery = "DELETE FROM " + comboBox.SelectedItem.ToString() + " WHERE ";
+
+                    List<string> conditions = new List<string>();
+
+                    foreach (string pk in primaryKeys)
+                    {
+                        string pkValue = row.Cells[pk].Value.ToString();
+                        conditions.Add(pk + " = '" + pkValue + "'");
+                    }
+
+                    deleteQuery += string.Join(" AND ", conditions);
+                    SqlCommand deleteCmd = new SqlCommand(deleteQuery, con);
+                    deleteCmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Record(s) deleted successfully!");
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM " + comboBox.SelectedItem.ToString(), con);
+                DataTable dt = new DataTable();
+                dataAdapter.Fill(dt);
+                dataGridView.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
             }
         }
     }
